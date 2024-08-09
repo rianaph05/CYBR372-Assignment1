5775962ee725c0e093efcb8412553f988dd3d508
rianaph05 <137390101+rianaph05@users.noreply.github.com>
2024-08-09 22:49:51 +1200

    Update Part4.java

--- assignment1/src/part4/Part4.java
+++ assignment1/src/part4/Part4.java
@@ -1,4 +1,3 @@
-
 package part4;
 
 import javax.crypto.*;
@@ -23,24 +22,24 @@ import java.util.logging.Logger;
  * java part4/Part4 ciphertext.enc -t 0
  * java part4/Part4 ciphertext.enc -t 1
  * java part4/Part4 ciphertext.enc -t 2
-*/
+ */
 
 public class Part4 {
     private static final Logger LOG = Logger.getLogger(Part4.class.getSimpleName());
     static String cipher = "AES/CBC/PKCS5PADDING";
     static String ALGORITHM = "AES";
     static String inFile = null;
+    private static final char[] ZERO_CHARS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final char[] ONE_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    private static final char[] TWO_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
     private static final int MAX_LENGTH = 6;
     static String foundPassword = null;
-    static String decryptedString = null;
     static byte[] salt;
     static byte[] iv;
     static byte[] ciphertext;
-    static char[] charSet;
 
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IOException, InvalidKeySpecException {
 
-        // converting command line args to scanner
         StringBuilder str = new StringBuilder();
         System.out.println("--------------------------------------------");
         System.out.println(" ** COMMAND LINE **");
@@ -53,26 +52,38 @@ public class Part4 {
         String input = str.toString();
         Scanner command = new Scanner(input);
 
-        // first arg is ciphertext file
         inFile = command.next();
         command.next();
-
-        // second arg is mode for password specifications
         int number = command.nextInt();
 
-        // pretty printing
         System.out.println(" ** CIPHERTEXT FILE **");
         System.out.println(inFile);
         System.out.println("--------------------------------------------");
+
         System.out.println(" ** PASSWORD PARAMETERS **");
         System.out.println("number:" + number);
 
-        parseCipherText();
+        Path path = Paths.get(inFile);
+        byte[] fileContent = Files.readAllBytes(path);
+        salt = Arrays.copyOfRange(fileContent, 0, 16);
+        iv = Arrays.copyOfRange(fileContent, 16, 32);
+        ciphertext = Arrays.copyOfRange(fileContent, 32, fileContent.length);
 
-        // generate char [] based off password specifications
-        charSet = initiateCharSet(number);
+        char[] charSet;
+        if (number == 0) {
+            System.out.println("password is at most 6 characters long, composed only of lowercase letters");
+            charSet = ZERO_CHARS;
+        } else if (number == 1) {
+            System.out.println("password is at most 6 characters long, composed only of lowercase letters and numbers");
+            charSet = ONE_CHARS;
+        } else if (number == 2) {
+            System.out.println("password is at most 6 characters long, composed only of lowercase and uppercase letters");
+            charSet = TWO_CHARS;
+        } else {
+            System.out.println("invalid number provided");
+            return;
+        }
 
-        // brute forcing password
         System.out.println("--------------------------------------------");
         System.out.println(" ** BRUTE FORCING... **");
         for (int length = 1; length <= MAX_LENGTH; length++) {
@@ -81,65 +92,14 @@ public class Part4 {
                 break;
             }
         }
-        // if password is found
         if (foundPassword != null) {
-            System.out.println("--------------------------------------------");
-            System.out.println(" ** PASSWORD FOUND **");
-            System.out.println("password: " + foundPassword);
-            System.out.println("decrypted ciphertext: " + decryptedString);
-            System.out.println("--------------------------------------------");
+            System.out.println("password found: " + foundPassword);
         } else {
             System.out.println("password not found");
         }
     }
 
-    /**
-     * Obtain salt, init vector and encrypted text from cipher text file
-     * @throws IOException if an I/O error occurs during decryption process
-     */
-    public static void parseCipherText() throws IOException {
-        Path path = Paths.get(inFile);
-        byte[] fileContent = Files.readAllBytes(path);
-        salt = Arrays.copyOfRange(fileContent, 0, 16);
-        iv = Arrays.copyOfRange(fileContent, 16, 32);
-        ciphertext = Arrays.copyOfRange(fileContent, 32, fileContent.length);
-    }
-
-    /**
-     * Initiate char array with possible password characters
-     * @param number password specifications
-     * @return char [] containing possible characters
-     */
-    public static char[] initiateCharSet(int number){
-        if (number == 0) {
-            System.out.println("password is at most 6 characters long, composed only of lowercase letters");
-            charSet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        } else if (number == 1) {
-            System.out.println("password is at most 6 characters long, composed only of lowercase letters and numbers");
-            charSet = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
-        } else if (number == 2) {
-            System.out.println("password is at most 6 characters long, composed only of lowercase and uppercase letters");
-            charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        } else {
-            System.out.println("invalid number provided");
-            return null;
-        }
-        return charSet;
-    }
-
-    /**
-     * Recursively generates and tries passwords
-     * @param charSet char [] containing possible password characters
-     * @param currentPassword char [] to store the current password being tested
-     * @param position current position in the password array to set the next character
-     * @throws NoSuchAlgorithmException if the algorithm for password decryption is not available
-     * @throws InvalidKeySpecException if the specification for the key is invalid
-     * @throws NoSuchPaddingException if the padding scheme for decryption is not available
-     * @throws InvalidAlgorithmParameterException if the parameters for the decryption algorithm are invalid
-     * @throws InvalidKeyException if the key used for decryption is invalid
-     * @throws IOException if an I/O error occurs during decryption process
-     */
-    public static void generateAndTryPassword(char[] charSet, char[] currentPassword, int position) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, IOException {
+    private static void generateAndTryPassword(char[] charSet, char[] currentPassword, int position) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, IOException {
         if (position == currentPassword.length) {
             return;
         }
@@ -158,19 +118,6 @@ public class Part4 {
         }
     }
 
-    /**
-     * Decrypting password using specified length
-     * @param password char [] of encrypted password to be decrypted
-     * @param length length of the password to be used
-     * @return true if the decryption is successful and the decrypted string contains mostly readable characters,
-     *         false otherwise
-     * @throws NoSuchAlgorithmException if the algorithm for password decryption is not available
-     * @throws InvalidKeySpecException if the specification for the key is invalid
-     * @throws NoSuchPaddingException if the padding scheme for decryption is not available
-     * @throws InvalidAlgorithmParameterException if the parameters for the decryption algorithm are invalid
-     * @throws InvalidKeyException if the key used for decryption is invalid
-     * @throws IOException if an I/O error occurs during  decryption process
-     */
     public static boolean decryptPassword(char[] password, int length) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, IOException {
         int count = 1000;
         PBEKeySpec pbeKeySpec = new PBEKeySpec(Arrays.copyOf(password, length), salt, count, 256);
@@ -183,8 +130,8 @@ public class Part4 {
 
         try {
             byte[] decrypted = pbeCipher.doFinal(ciphertext);
-            decryptedString = new String(decrypted);
-
+            String decryptedString = new String(decrypted);
+            System.out.println("decrypted ciphertext: " + decryptedString);
             // check if string contains mostly readable chars
             for (char c : decryptedString.toCharArray()){
                 if (c < 32 || c > 126){
@@ -198,4 +145,3 @@ public class Part4 {
         }
     }
 }
-
